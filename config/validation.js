240d8e2724febf9a5e6e40380263e3ba327392e0
adelianjelina <adelianjelina@mhs.mdp.ac.id>
2023-06-22 13:17:14 +0700

    apdett

--- config/validation.js
+++ config/validation.js
@@ -12,7 +12,7 @@ const registerValidation = (data) => {
 
 const loginValidation = (data) => {
     const schema = Joi.object({
-        usernama: Joi.string().required(),
+        username: Joi.string().required(),
         password: Joi.string().min(6).required()
     })
 
